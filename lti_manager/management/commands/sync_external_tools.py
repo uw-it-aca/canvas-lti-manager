a27363777122f903cd84571f885e1a08635b0a86
Jim Laney <jlaney@uw.edu>
2016-05-10 13:30:56 -0700

    fix account_id

--- lti_manager/management/commands/sync_external_tools.py
+++ lti_manager/management/commands/sync_external_tools.py
@@ -1,9 +1,12 @@
 from django.conf import settings
+from django.utils.timezone import utc
 from sis_provisioner.management.commands import SISProvisionerCommand
 from restclients.canvas.external_tools import ExternalTools
 from restclients.canvas.accounts import Accounts
 from restclients.exceptions import DataFailureException
 from lti_manager.models import ExternalTool, ExternalToolAccount
+from datetime import datetime
+import json
 
 
 class Command(SISProvisionerCommand):
@@ -20,14 +23,15 @@ class Command(SISProvisionerCommand):
         self.update_job()
 
     def update_tools_in_account(self, account):
-        for tool in self._tools.get_external_tools_in_account(account_id):
+        for tool in self._tools.get_external_tools_in_account(
+                account.account_id):
             self.update_model(account, tool)
 
         for subaccount in self._accounts.get_sub_accounts(account.account_id):
             self.update_tools_in_account(subaccount)
 
     def update_model(self, account, config):
-        canvas_id = config.id
+        canvas_id = config['id']
         try:
             external_tool = ExternalTool.objects.get(canvas_id=canvas_id)
         except ExternalTool.DoesNotExist:
@@ -38,10 +42,12 @@ class Command(SISProvisionerCommand):
                 account_id=account.account_id)
         except ExternalToolAccount.DoesNotExist:
             et_account = ExternalToolAccount(account_id=account.account_id)
-            et.sis_account_id = account.sis_account_id
+            et_account.sis_account_id = account.sis_account_id
             et_account.name = account.name
             et_account.save()
 
         external_tool.account = et_account
         external_tool.config = json.dumps(config)
+        external_tool.changed_by = 'auto'
+        external_tool.changed_date = datetime.utcnow().replace(tzinfo=utc)
         external_tool.save()
